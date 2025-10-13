ee36c81dd3eeea017182c3a27c93fc58deef8a00
Thomas Durlacher <74486397+durlachert@users.noreply.github.com>
2025-10-13 14:53:32 +0200

    Update stg_orders.sql

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,10 +1,9 @@
 {{ config(materialized='view') }}
 
-
 select
-o.ORDER_ID,
-o.CUSTOMER_ID,
-o.ORDER_DATE,
-o.ORDER_STATUS,
-o.TOTAL_AMOUNT
-from {{ source('raw', 'orders') }} o
+  o.ORDER_ID,
+  o.CUSTOMER_ID,
+  o.ORDER_DATE,
+  o.ORDER_STATUS,
+  o.TOTAL_AMOUNT
+from {{ source('raw', 'orders') }} as o
