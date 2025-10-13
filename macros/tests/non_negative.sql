d3fb35dfc4266a04a888c4a56d5e10f2c780aa1d
Thomas Durlacher <74486397+durlachert@users.noreply.github.com>
2025-10-13 15:14:28 +0200

    Create non_negative.sql

---
+++ macros/tests/non_negative.sql
@@ -0,0 +1,12 @@
+{% test non_negative(model, column_name) %}
+
+with validation as (
+  select
+    {{ column_name }} as value
+  from {{ model }}
+  where {{ column_name }} < 0
+)
+
+select * from validation
+
+{% endtest %}
