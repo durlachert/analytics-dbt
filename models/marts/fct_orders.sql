960dafb6813169954138012f78d7f8a01cc33a7d
Thomas Durlacher <74486397+durlachert@users.noreply.github.com>
2025-10-13 14:53:44 +0200

    Update fct_orders.sql

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,9 +1,10 @@
 {{ config(materialized='table') }}
 
-
 select
-CUSTOMER_ID,
-count(*) as order_count,
-sum(TOTAL_AMOUNT) as total_spend
+  CUSTOMER_ID,
+  count(*)               as order_count,
+  sum(TOTAL_AMOUNT)      as total_spend,
+  min(ORDER_DATE)        as first_order_date,
+  max(ORDER_DATE)        as last_order_date
 from {{ ref('stg_orders') }}
 group by 1
